59ec0e50e4abae8c9cd9720d390db6e9d6a6927d
Oluwatosin Olufon <Oluwatosin2.Olufon@live.uwe.ac.uk>
2023-05-18 12:25:27 +0100

    Added the Debug.Log on Thumbs Up Pose

---
+++ Assets/ThumbsUpAction.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThumbsUpAction : MonoBehaviour
+{
+
+    public void OnDetection()
+    { Debug.Log("Thumb Pose detected"); }
+}
