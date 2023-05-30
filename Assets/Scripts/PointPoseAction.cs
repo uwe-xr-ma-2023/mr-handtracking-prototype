9b170e7578b34cd2fcd8efb4084640b14eeb3d13
Jon Higgins <jon@jonhiggins.co.uk>
2023-05-30 17:18:59 +0100

    P1C Toggle trail renderer with gesture

--- Assets/Scripts/PointPoseAction.cs
+++ Assets/Scripts/PointPoseAction.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class PointPoseAction : MonoBehaviour
 {
     private AudioSource audioSource;
+    public TrailRenderer trailRenderer;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        trailRenderer.enabled = false;
     }
     public void OnPoseStart()
     { 
         Debug.Log("Point Pose started");
         audioSource.Play();
+        trailRenderer.enabled = true;
     }
 
     public void OnPoseEnd()
     {
         Debug.Log("Point Pose ended");
         audioSource.Play();
+        trailRenderer.enabled = false;
+        trailRenderer.Clear();
     }
 }
