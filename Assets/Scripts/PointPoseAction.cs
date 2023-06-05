16c2bb1be36db349219c83bb8aac706b5a5e90e2
Jon Higgins <jon@jonhiggins.co.uk>
2023-06-05 17:01:24 +0100

    Add PDollar for shape recognition

--- Assets/Scripts/PointPoseAction.cs
+++ Assets/Scripts/PointPoseAction.cs
@@ -1,29 +1,70 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using PDollarGestureRecognizer;
+
 
 public class PointPoseAction : MonoBehaviour
 {
     private AudioSource audioSource;
     public TrailRenderer trailRenderer;
+    private List<Gesture> trainingSet = new List<Gesture>();
+    public TMP_Text debugText;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         trailRenderer.enabled = false;
+
+        TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
+        foreach (TextAsset gestureXml in gesturesXml)
+            trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
     }
     public void OnPoseStart()
     { 
-        Debug.Log("Point Pose started");
         audioSource.Play();
         trailRenderer.enabled = true;
     }
 
     public void OnPoseEnd()
     {
-        Debug.Log("Point Pose ended");
         audioSource.Play();
         trailRenderer.enabled = false;
+        string gesture = RecogniseGesture();
         trailRenderer.Clear();
+    }
+
+    List<Point> BuildPointsArray()
+    {
+        List<Point> points = new List<Point>();
+        // Get positions from ink trail renderer
+        Vector3[] trailRecorded = new Vector3[trailRenderer.positionCount];
+        int positions = trailRenderer.GetPositions(trailRecorded);
+        // Add PPDollar Point objects for each position on ink to points array
+        foreach (Vector3 trail in trailRecorded)
+        {
+            points.Add(new Point(trail.x, -trail.y, 0));
+        }
+        return points;
+    }
+
+    string RecogniseGesture()
+    {
+        List<Point> points = BuildPointsArray();
+        // User hasn't drawn enough points with inkTrail
+        if (points.Count < 10)
+        {
+            return "";
+        }
+        // Create PDollar gesture and classify against existing gestures
+        Gesture candidate = new Gesture(points.ToArray());
+        Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
+
+        string resultString = gestureResult.GestureClass + " " + gestureResult.Score;
+        if (debugText)
+        {
+            debugText.text = resultString;
+        }
+        return gestureResult.Score > 0.8 ? gestureResult.GestureClass : "";
     }
 }
